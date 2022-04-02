af8ee1b520f5d96eaca3642fd8547cab7a1cbe46
Chris King <chris.aa.king@gmail.com>
2022-04-02 04:23:10 +0200

    better responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   mode: "jit",
   content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      xs: "465px",
+      "600bp": "600px",
+    },
+  },
 };
